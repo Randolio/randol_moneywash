0e58cd6ba978abb8416b22b52fdebfb77cd64494
Randolio <103154494+Randolio@users.noreply.github.com>
2024-02-21 03:21:09 +0000

    refactor(points): use points for spawn/delete peds.

--- cl_moneywash.lua
+++ cl_moneywash.lua
@@ -1,37 +1,45 @@
 local Config = {}
-local pedTable = {}
+local MW_PED = {}
+local storedPoints = {}
 
 local function deleteAllPeds()
-    for i = 1, #pedTable do
-        if DoesEntityExist(pedTable[i]) then
-            DeleteEntity(pedTable[i])
+    for point, _ in pairs(storedPoints) do
+        if storedPoints[point] then
+            storedPoints[point]:remove()
         end
     end
-    table.wipe(pedTable)
+    for ped, _ in pairs(MW_PED) do
+        if DoesEntityExist(MW_PED[ped]) then
+            DeleteEntity(MW_PED[ped])
+            exports['qb-target']:RemoveTargetEntity(MW_PED[ped], "Exchange")
+        end
+    end
+    table.wipe(MW_PED)
+    table.wipe(storedPoints)
     table.wipe(Config)
 end
 
-local function spawnPeds()
-    for i = 1, #Config.locations do
-        local data = Config.locations[i]
+local function spawnPed(point)
+    if not DoesEntityExist(MW_PED[point.index]) then
+        local data = point.pedData
         local model = joaat(data.model)
         lib.requestModel(model, 5000)
-
-        local MW_PED = CreatePed(6, model, data.coords.x, data.coords.y, data.coords.z - 1.0, data.coords.w, false, true)
-        SetEntityAsMissionEntity(MW_PED)
-        SetPedFleeAttributes(MW_PED, 0, 0)
-        SetBlockingOfNonTemporaryEvents(MW_PED, true)
-        SetEntityInvincible(MW_PED, true)
-        FreezeEntityPosition(MW_PED, true)
+        
+        MW_PED[point.index] = CreatePed(6, model, data.coords.x, data.coords.y, data.coords.z - 1.0, data.coords.w, false, true)
+        SetEntityAsMissionEntity(MW_PED[point.index])
+        SetPedFleeAttributes(MW_PED[point.index], 0, 0)
+        SetBlockingOfNonTemporaryEvents(MW_PED[point.index], true)
+        SetEntityInvincible(MW_PED[point.index], true)
+        FreezeEntityPosition(MW_PED[point.index], true)
 
         if data.dict then
             lib.requestAnimDict(data.dict, 5000)        
-            TaskPlayAnim(MW_PED, data.dict, data.anim, 8.0, 1.0, -1, 01, 0, 0, 0, 0)
+            TaskPlayAnim(MW_PED[point.index], data.dict, data.anim, 8.0, 1.0, -1, 01, 0, 0, 0, 0)
         elseif data.scenario then
-            TaskStartScenarioInPlace(MW_PED, data.scenario, 0, true)
+            TaskStartScenarioInPlace(MW_PED[point.index], data.scenario, 0, true)
         end
 
-        exports['qb-target']:AddTargetEntity(MW_PED, { -- ox-target supports qb-target conversion for qb and esx
+        exports['qb-target']:AddTargetEntity(MW_PED[point.index], { -- ox-target supports qb-target conversion for qb and esx
             options = {
                 {
                     icon = 'fa-solid fa-sack-dollar',
@@ -46,8 +54,28 @@ local function spawnPeds()
             },
             distance = 1.5,
         })
-        pedTable[#pedTable+1] = MW_PED
     end
+end
+
+local function yeetPed(point)
+    if DoesEntityExist(MW_PED[point.index]) then
+        DeleteEntity(MW_PED[point.index])
+        exports['qb-target']:RemoveTargetEntity(MW_PED[point.index], "Exchange")
+        MW_PED[point.index] = nil
+    end
+end
+
+local function createPoints()
+    for id, data in pairs(Config.locations) do
+		storedPoints[id] = lib.points.new({
+			coords = data.coords,
+			distance = 30,
+			index = id,
+            pedData = data,
+            onEnter = spawnPed,
+			onExit = yeetPed,
+		})
+	end
 end
 
 RegisterNetEvent('randol_moneywash:client:exchangeBills', function()
@@ -69,7 +97,7 @@ end)
 RegisterNetEvent('randol_moneywash:client:cacheConfig', function(data)
     if GetInvokingResource() or not hasPlyLoaded() then return end
     Config = data
-    spawnPeds()
+    createPoints()
 end)
 
 AddEventHandler('onResourceStop', function(resourceName) 
